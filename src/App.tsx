058089840738eceb5478fa00f203a991d59573e7
parkjeehoon <gkstj8300@naver.com>
2025-02-14 11:25:14 +0900

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello, Prography 10th Frontend!</h1>
+        </div>
+    );
+};
+
+export default App;
