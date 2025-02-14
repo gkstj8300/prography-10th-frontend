7580f9246cdbe062203cf12ed48ee0cf992881c9
parkjeehoon <gkstj8300@naver.com>
2025-02-14 13:38:24 +0900

    package library setting

---
+++ .eslintrc.js
@@ -0,0 +1,86 @@
+const defaultRestrictedImportPatterns = ['*../**'];
+
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['unused-imports', 'sort-keys-fix'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:jsx-a11y/recommended',
+	],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+		'import/resolver': 'webpack',
+	},
+	env: {
+		browser: true,
+		amd: true,
+		node: true,
+	},
+	rules: {
+		curly: 'error',
+		'lines-between-class-members': ['error', 'always'],
+		'member-access': 'off',
+		'no-console': 'error',
+		'no-debugger': 'error',
+		'no-dupe-keys': 'error',
+		'no-eval': 'error',
+		'no-restricted-imports': [
+			'error',
+			{
+				patterns: defaultRestrictedImportPatterns,
+			},
+		],
+		'no-restricted-syntax': ['error', 'TSEnumDeclaration'],
+		'padding-line-between-statements': [
+			'error',
+			{
+				blankLine: 'always',
+				prev: 'function',
+				next: 'function',
+			},
+			{
+				blankLine: 'always',
+				prev: 'block-like',
+				next: 'block-like',
+			},
+		],
+		'spaced-comment': [
+			'error',
+			'always',
+			{
+				line: {
+					exceptions: ['-', '='],
+				},
+				block: {
+					balanced: true,
+				},
+				markers: ['/'],
+			},
+		],
+		'react/display-name': 'off',
+		'unused-imports/no-unused-imports': 'error',
+	},
+	overrides: [
+		{
+			files: ['**/*.ts?(x)'],
+			rules: {
+				'no-any': 'off',
+				'no-dupe-class-members': 'off',
+				'@typescript-eslint/explicit-member-accessibility': [
+					'error',
+					{
+						accessibility: 'no-public',
+					},
+				],
+				'@typescript-eslint/explicit-module-boundary-types': 'off',
+				'@typescript-eslint/no-empty-interface': 0,
+				'@typescript-eslint/no-explicit-any': 'error',
+			},
+		},
+	],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const App: React.FC = () => {
-    return (
-        <div>
-            <h1>Hello, Prography 10th Frontend!</h1>
-        </div>
-    );
+	return (
+		<div>
+			<h1>Hello, Prography 10th Frontend!</h1>
+		</div>
+	);
 };
 
 export default App;
